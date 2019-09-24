49490a0b77a32ec93301d248410f931619dbe6a4
Aaron Chen Desktop <chenaaron3@gmail.com>
2019-09-24 02:43:08 -0700

    first commit from server

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+const sgMail = require('@sendgrid/mail');
+
+/* GET home page. */
+router.post('/email', function(req, res, next) {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  console.log(req.body);
+  const msg = {
+    to: req.body.to,
+    from: req.body.from,
+    subject: req.body.subject,
+    text: req.body.text
+  };
+  sgMail.send(msg).then(function() {
+    res.status(200).send("Message sent!");
+  }).catch(function(){
+    res.status(400).send("Please enter a valid email!");
+  })
+});
+
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+module.exports = router;
