e70fe4762bfa0989dbb97da5336b90b01154920a
Aaron Chen Desktop <chenaaron3@gmail.com>
2019-09-24 19:29:42 -0700

    added external link to project

--- client/src/Componenets/Project.jsx
+++ client/src/Componenets/Project.jsx
@@ -21,6 +21,9 @@ class Project extends Component {
 
         return (
             <div className={`project ${activeVal}`} data-aos={this.state.opened ? "" : "zoom-in-up"}>
+                <svg className="project-external-link">
+                    <path fill="#000000" d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z" />
+                </svg>git
                 <h1 className="project-title">{this.props.title}</h1>
                 {
                     this.state.opened &&
