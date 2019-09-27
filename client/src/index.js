acd2716755156cd4f7393d59d37a3ebd2c2477c7
Aaron Chen Desktop <chenaaron3@gmail.com>
2019-09-27 14:18:50 -0700

    prevents heroku from falling asleep

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,11 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+var http = require("http");
+setInterval(function() {
+    http.get("http://achen-website.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
