79e15e49ea7654da3a3e1962c27a301f65b2e19b
Aaron Chen Laptop <chenaaron3@gmail.com>
2020-06-09 15:04:49 -0700

    using spawn instead of exec

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -11,20 +11,35 @@ router.post("/github", function (req, res) {
     var sender = req.body.sender;
     var branch = req.body.ref;
 
-    console.log(sender, branch);
-    if(branch.indexOf('master') > -1 && sender.login === githubUsername){
+    console.log("Sender:", sender, "Branch:", branch);
+    if (branch.indexOf('master') > -1 && sender.login === githubUsername) {
+        console.log("Push Detected! Now Deploying!");
         deploy(res);
     }
 })
 
-function deploy(res){
-    childProcess.exec(`cd ${scriptDirectory} && ./${scriptFile}`, function(err, stdout, stderr){
-        if (err) {
-         console.error(err);
-         return res.send(500);
-        }
+function deploy(res) {
+    let command = `cd ${scriptDirectory} && ./${scriptFile}`;
+    const child = spawn(command, {
+        stdio: 'inherit',
+        shell: true
+    });
+
+    child.on('exit', function (code, signal) {
+        console.log('child process exited with ' + `code ${code} and signal ${signal}`);
         res.send(200);
-      });
+
+    });
+
+    // childProcess.exec(command, function (err, stdout, stderr) {
+    //     if (err) {
+    //         console.log("Something went wrong during deployment!")
+    //         console.error(err);
+    //         return res.send(500);
+    //     }
+    //     console.log("Successfully Deployed!")
+    //     res.send(200);
+    // });
 }
 
 module.exports = router;
