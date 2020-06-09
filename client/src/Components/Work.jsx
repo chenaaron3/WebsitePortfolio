23253c1b5acebb7f4cdb838e2977edbc895130c0
Aaron Chen Laptop <chenaaron3@gmail.com>
2020-06-09 16:04:30 -0700

    consistent interactions

--- client/src/Components/Work.jsx
+++ client/src/Components/Work.jsx
@@ -1,23 +1,25 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./Work.scss";
 
 class Work extends Component {
     render() {
         return (
-            <div className="work-card" data-aos="zoom-in-up">
-                <h1 className="work-position">{this.props.pos}</h1>
-                <a href={this.props.companyLink} className="work-company" target="_blank" rel="noopener noreferrer">
-                    @ {this.props.company}
-                </a>
-                <h3 className="work-duration">{this.props.fromto}</h3>
-                <ul>
-                    {
-                        this.props.responsibilities.map((responsibility, index) =>
-                            <li key={index}>
-                                {responsibility}
-                            </li>)
-                    }
-                </ul>
+            <div className="work-card-wrapper" data-aos="zoom-in-up">
+                <div className="work-card">
+                        <h1 className="work-position">{this.props.pos}</h1>
+                        <a href={this.props.companyLink} className="work-company" target="_blank" rel="noopener noreferrer">
+                            @ {this.props.company}
+                        </a>
+                        <h3 className="work-duration">{this.props.fromto}</h3>
+                        <ul>
+                            {
+                                this.props.responsibilities.map((responsibility, index) =>
+                                    <li key={index}>
+                                        {responsibility}
+                                    </li>)
+                            }
+                        </ul>
+                </div>
             </div>
         );
     }
