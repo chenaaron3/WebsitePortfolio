6c1b5ca5aa4f258600abc15e36e4f31a2a215ca0
Aaron Chen Laptop <chenaaron3@gmail.com>
2020-09-15 23:12:22 -0700

    added l3harris to experience

--- client/src/Components/Projects.jsx
+++ client/src/Components/Projects.jsx
@@ -9,7 +9,7 @@ class Projects extends Component {
             webDev: [],
             systems: []
         }
-        let endpoints = ["/projects/webDev/peterPortal", "/projects/webDev/portfolio", "/projects/webDev/allergize", "/projects/webDev/stocks",
+        let endpoints = ["/projects/webDev/stocks", "/projects/webDev/peterPortal", "/projects/webDev/portfolio", "/projects/webDev/allergize",
             "/projects/systems/allocator", "/projects/systems/shell", "/projects/systems/pintos"];
         endpoints.forEach(endpoint => {
             fetch(endpoint)
